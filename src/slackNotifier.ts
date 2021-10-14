f9703a02157dece1eb47cd5007cb3d9d725f9ed6
sgs-turbo23 <masiwasaki1006@gmail.com>
2021-10-14 05:47:30 +0000

    update node-ci.yml

--- src/slackNotifier.ts
+++ src/slackNotifier.ts
@@ -8,18 +8,16 @@ class slackNotifier {
   }
 
   postToSlack(message: string): void {
-    const jsonData =
-    {
-      "username" : this.username,
-      "text" : message
+    const jsonData = {
+      username: this.username,
+      text: message,
     };
     const payload = JSON.stringify(jsonData);
 
-    const options =
-    {
-      "method" : "post",
-      "contentType" : "application/json",
-      "payload" : payload
+    const options = {
+      method: "post",
+      contentType: "application/json",
+      payload: payload,
     };
 
     UrlFetchApp.fetch(this.postUrl, options);
